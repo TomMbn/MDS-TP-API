99ee9021ceb7c8b5b89a97491c5b47803d14f6ef
Tom <tom.mauboussin@icloud.com>
2025-01-30 13:38:25 +0100

    Fix : connexion base de données

--- src/config/database.js
+++ src/config/database.js
@@ -10,7 +10,8 @@ export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,
       trustServerCertificate: true,
       enableArithAbort: true
     }
-  }
+  },
+  port: process.env.DB_
 });
 
 
