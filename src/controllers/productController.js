b71cff385e821735413993186c2e6e5fe476ce7d
Tom <tom.mauboussin@icloud.com>
2025-01-30 21:50:26 +0100

    Fix : userController

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,11 +1,41 @@
 import Product from "../models/products.js";
 
 export const createProduct = async (req, res) => {
+  const {
+    marque,
+    nom_modele,
+    description,
+    type,
+    prix,
+    stock,
+    id_fournisseur
+  } = req.body;
+
   try {
-    const product = await Product.create(req.body);
-    res.status(201).json(product);
+    const existingProduct = await Product.findOne({ where: { nom_modele } });
+    if (existingProduct) {
+      return res.status(400).send("Le produit existe déjà");
+    }
+
+    const newProduct = await Product.create({
+      marque,
+      nom_modele,
+      description,
+      type,
+      prix,
+      stock,
+      id_fournisseur,
+    });
+
+    return res.status(201).json({
+      id_produit: newProduct.id_produit,
+      marque: newProduct.marque,
+      nom_modele: newProduct.nom_modele,
+      type: newProduct.type,
+    });
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    console.error("Erreur lors de la création du produit:", error);
+    res.status(500).send("Erreur interne du serveur");
   }
 };
 export const getAllProducts = async (req, res) => {
@@ -13,41 +43,67 @@ export const getAllProducts = async (req, res) => {
     const products = await Product.findAll();
     res.status(200).json(products);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.error("Erreur lors de la récupération des produits:", error);
+    res.status(500).send("Erreur interne du serveur");
   }
 };
 export const getProductById = async (req, res) => {
+  const { id } = req.params;
+
   try {
-    const product = await Product.findByPk(req.params.id);
+    const product = await Product.findByPk(id);
     if (!product) {
-      return res.status(404).json({ error: "Produit non trouvé" });
+      return res.status(404).send("Produit non trouvé");
     }
+
     res.status(200).json(product);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.error("Erreur lors de la récupération du produit:", error);
+    res.status(500).send("Erreur interne du serveur");
   }
 };
 export const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const { marque, nom_modele, description, type, prix, stock, id_fournisseur } =
+    req.body;
+
   try {
-    const product = await Product.findByPk(req.params.id);
+    const product = await Product.findByPk(id);
     if (!product) {
-      return res.status(404).json({ error: "Produit non trouvé" });
+      return res.status(404).send("Produit non trouvé");
     }
-    await product.update(req.body);
-    res.status(200).json(product);
+
+    await product.update({
+      marque,
+      nom_modele,
+      description,
+      type,
+      prix,
+      stock,
+      id_fournisseur,
+    });
+
+    res
+      .status(200)
+      .json({ message: "Produit mis à jour avec succès", product });
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    console.error("Erreur lors de la mise à jour du produit:", error);
+    res.status(500).send("Erreur interne du serveur");
   }
 };
 export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+
   try {
-    const product = await Product.findByPk(req.params.id);
+    const product = await Product.findByPk(id);
     if (!product) {
-      return res.status(404).json({ error: "Produit non trouvé" });
+      return res.status(404).send("Produit non trouvé");
     }
+
     await product.destroy();
     res.status(204).send();
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.error("Erreur lors de la suppression du produit:", error);
+    res.status(500).send("Erreur interne du serveur");
   }
 };
